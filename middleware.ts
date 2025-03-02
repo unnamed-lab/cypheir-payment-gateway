8ddd9db39f9b6d7ce53de83a7cd0c88e925b45ff
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-02 23:53:03 +0100

    Add authentication middleware configuration and update User model; introduce Milestone model for invoice tracking

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
 export { auth as middleware } from "@/lib/auth";
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/create-invoice/:path*",
+    "/customers/:path*",
+    "/invoices/:path*",
+    "/api/:path*",
+    "/analytics/:path*",
+  ],
+};
